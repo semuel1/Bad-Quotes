9984555ee0c90de6f8cc761e8c1aae1a3c22d6e0
semuel1 <semuelst1@gmail.com>
2021-03-03 08:56:46 -0800

    updates

---
+++ server.js
@@ -0,0 +1,11 @@
+// Modules and variables
+const express = require('express')
+const ejslayouts = require('express-ejs-layouts')
+const rowdy = require('rowdy-logger')
+
+const app = express()
+//Controllers
+
+
+// Routes
+
