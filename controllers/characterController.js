0b5242f67ae39b77553fcbe90f3761a0904a343d
semuel1 <semuelst1@gmail.com>
2021-03-08 09:45:16 -0800

    update

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,11 +1,20 @@
 const router = require('express').Router()
+const { default: axios } = require('axios')
 const db = require('../models')
 
-router.post('/' ,(req, res) => {
-  
-
-    res.redirect('')
+router.post('/', async (req, res) => {
+    try {
+        const [newCharacter, created] = await db.badCharacter.findOrCreate({
+            where: { 
+                name: req.body.name 
+            }
+        })
+        // console.log(created);
+        res.locals.user.addCharacter(newCharacter);
+        res.redirect(`/results`)
+    } catch (err) {
+        console.log(err)
+    }
 })
-
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // Modules and variables
+require('dotenv').config()
 const express = require('express')
 const rowdy = require('rowdy-logger')
 const axios = require('axios')
 const morgan = require('morgan')
+const methodOverride = require('method-override')
 const ejsLayouts = require('express-ejs-layouts')
 const cryptoJS = require('crypto-js')
+const bcrypt = require('bcrypt')
 const db = require('./models')
 
 
@@ -12,20 +15,44 @@ const app = express()
 const rowdyRes = rowdy.begin(app)
 const PORT = process.env.PORT || 3000
 //Middleware
-app.use(morgan('tiny'))
+app.use(morgan('dev'))
 app.set('view engine', 'ejs')
 app.use(require('cookie-parser')())
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended:false }))
 app.use(ejsLayouts)
 
 
+// Routes
+app.use(async (req, res, next) => {
+    console.log(req.cookies)
+    if (req.cookies.userId != 'undefined') {
+        const decryptedId = cryptoJS.AES.decrypt(req.cookies.userId, process.env.COOKIE_SECRET).toString(cryptoJS.enc.Utf8)
+        
+        
+        // const user = await db.user.findByPk(decryptedId)
+        const user = await db.user.findOne({
+            where: {
+                id: decryptedId
+            }
+        })
+        
+        res.locals.user = user
+    } else {
+        res.locals.user = null
+    }
+    
+    next()
+})
+
 // //Controllers
 app.use('/users', require('./controllers/userController'))
 app.use('/characters', require('./controllers/characterController'))
 
-
-// Routes
+app.get('/profile', async (req, res) => {
+    res.render('users/profile')
+})
 
 app.get('/results', async (req, res) => {
     // /Axios.get to BB API - req.body.(nameofinput) - results of axios get put into results
@@ -52,7 +79,6 @@ app.get('/results', async (req, res) => {
     }
     
 })
-
 
 app.get('/', (req, res) => {
     res.render('index')
